46e18b370c8496219c1926c7b2f01eae5d10e0bb
rutikrokade <rutikrokade5084@gmail.com>
2024-12-19 09:54:36 +0530

    repo init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Products from "./Pages/Products";
+import './App.css';
+import { Routes } from "react-router";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Routes>
+
+      </Routes> */}
+      Twinleaves/Pallet
+      <Products />
+    </div>
+  );
+}
+
+export default App;
